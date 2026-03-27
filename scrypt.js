6e0c5295b755f030046f65f116217c9b3efdf950
Didi-show <gsm22@aluno.ifnmg.edu.br>
2026-03-27 02:20:19 +0000

    fim

--- scrypt.js
+++ scrypt.js
@@ -4,12 +4,19 @@ const backBtn = document.getElementById("back-btn");
 const mainPage = document.getElementById("main-page");
 const secretPage = document.getElementById("secret-page");
 
+const musica = document.getElementById("musica");
+
 nextBtn.addEventListener("click", () => {
     mainPage.classList.add("hidden");
     secretPage.classList.remove("hidden");
+
+    musica.play(); // ▶️ começa a música
 });
 
 backBtn.addEventListener("click", () => {
     secretPage.classList.add("hidden");
     mainPage.classList.remove("hidden");
+
+    musica.pause(); // ⏸️ pausa a música (opcional)
+    musica.currentTime = 0; // volta pro começo (opcional)
 });
